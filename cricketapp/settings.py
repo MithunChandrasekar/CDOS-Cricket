88279c862e9b01914072b077053769977f54e401
EC2 Default User <ec2-user@ip-172-31-3-31.eu-west-1.compute.internal>
2024-12-08 21:18:34 +0000

    final commit before submission

--- cricketapp/settings.py
+++ cricketapp/settings.py
@@ -143,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='') 
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'djangomitz@gmail.com'
